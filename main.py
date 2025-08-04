7733acf20b79b574349cea23eba7a15a7074df9b
Eb1su <higginsjamie3@gmail.com>
2025-08-04 21:23:07 +0100

    added ability to shoot and destroy asteroids

--- main.py
+++ main.py
@@ -4,15 +4,21 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -30,10 +36,15 @@ def main():
                 return
 
         updatable.update(dt)
+
         for ast in asteroids:
             if ast.collision_check(player) == True:
                 print("Game over!")
                 sys.exit()
+            
+            for shot in shots:
+                if shot.collision_check(ast) == True:
+                    ast.kill()
         
 
         pygame.Surface.fill(screen, "black")
