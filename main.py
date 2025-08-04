9f3095421371b6509eba25c8ad8eaa61dfbb0402
Eb1su <higginsjamie3@gmail.com>
2025-08-04 17:45:22 +0100

    Added framerate tick to save on resources, created circleshape.py for game sprites

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import pygame
 from constants import *
 
 def main():
-    pygame.init
+    pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    frame_rate = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -14,6 +18,8 @@ def main():
 
         pygame.Surface.fill(screen, "black")
         pygame.display.flip()
+        frame_rate.tick(60)
+        dt = (frame_rate.tick()) / 1000
 
 
 
